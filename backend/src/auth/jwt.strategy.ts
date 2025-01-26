69cece1ff4ba55d1862fc0615fc2fb2146f566dc
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2025-01-25 22:51:41 -0300

    something

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,19 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly configService: ConfigService) {
+  constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: configService.get<string>(process.env.JWT_SECRET),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // Extrai o token do header Authorization
+      ignoreExpiration: false, // Rejeita tokens expirados
+      secretOrKey: process.env.JWT_SECRET, // Pega a chave secreta do .env
     });
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+    return { userId: payload.sub, username: payload.username }; // Retorna dados do usuário
   }
 }
