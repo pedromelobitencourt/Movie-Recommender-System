7bc523c8763303ee7dff16d730671200fa5d621f
SergioHenrique23 <serginho.abreu@outlook.com>
2025-02-02 12:45:16 -0300

    initial 2 front-end commit

--- frontend/streamconnect/src/components/Header.tsx
+++ frontend/streamconnect/src/components/Header.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import './style/HeaderStyle.css';
 
 const Header: React.FC = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <header>
-      <Navbar bg="transparent" expand="md" fixed="top" className="navbar-transparente">
+      <Navbar
+        bg="transparent"
+        expand="md"
+        fixed="top"
+        className={`navbar-transparente ${scrolled ? 'scrolled' : ''}`}
+      >
         <Container>
           <Navbar.Brand href="/">
-            <img src="/imagens/logo.png" width="68" alt="Logo" />
+            <img src="/images/logo.png" width="68" alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="nav-principal">
             <FontAwesomeIcon icon={faBars} className="text-white" />
           </Navbar.Toggle>
           <Navbar.Collapse id="nav-principal">
             <Nav className="ml-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="#">Ajuda</Nav.Link>
+              <Nav.Link href="/" className="nav-link">Home</Nav.Link>
+              <Nav.Link href="#" className="nav-link">Ajuda</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
