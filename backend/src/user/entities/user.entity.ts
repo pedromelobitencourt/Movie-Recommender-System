51cf2ff2e86e00d3adb769e174529d10405a4702
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2025-02-03 14:34:17 -0300

    update user

--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -14,39 +14,6 @@ export class User {
   @Column() // Senha do usuário
   password: string;
 
-  @Column({ nullable: true }) // Idade
-  age: number;
-
-  @Column({ nullable: true }) // Gênero
-  gender: string;
-
-  @Column({ nullable: true }) // Localização
-  location: string;
-
   @Column({ default: () => 'CURRENT_TIMESTAMP' }) // Data de criação
   createdAt: Date;
-
-  @Column('text', { array: true, nullable: true }) // Gêneros favoritos do usuário
-  preferredGenres: string[];
-
-  @Column({ default: false }) // Usuário aceita recomendações personalizadas?
-  acceptsRecommendations: boolean;
-
-  @Column({ nullable: true }) // Cluster ao qual o usuário pertence (se houver clustering de usuários)
-  clusterId: number;
-
-  @Column('float', { array: true, nullable: true }) // Vetores de características do usuário para recomendação
-  featureVector: number[];
-
-  @Column({ nullable: true }) // Último login do usuário
-  lastLogin: Date;
-
-  @Column({ nullable: true }) // IP do último acesso
-  lastIp: string;
-
-  @Column({ default: false }) // Conta está ativa?
-  isActive: boolean;
-
-  @Column({ default: false }) // Usuário tem acesso como administrador?
-  isAdmin: boolean;
 }
