78eadd479085b2bd05cdf662ea6019dde693c5af
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2025-02-03 17:19:33 -0300

    add movie recommender route

--- ml-api/app/main.py
+++ ml-api/app/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 from routes.clustering import router as clustering_router
+from routes.prediction import router as prediction_router
 
-# Inicializa a aplicação FastAPI
 app = FastAPI(title="Movie Clustering API", version="1.0")
 
-# Inclui as rotas relacionadas ao clustering
+app.include_router(prediction_router)
 app.include_router(clustering_router)
 
-# Executar o servidor
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8111)
