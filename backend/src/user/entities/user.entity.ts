807d646bf2c1fb142baa3cf263f0da0808d32c70
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2025-02-03 14:11:07 -0300

    change user entity

--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -2,7 +2,7 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity() // Indica que esta classe é uma entidade do banco de dados
 export class User {
-  @PrimaryGeneratedColumn('uuid') // Chave primária gerada automaticamente
+  @PrimaryGeneratedColumn('increment') // Chave primária gerada automaticamente
   id: string;
 
   @Column() // Nome do usuário
