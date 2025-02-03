0592d3f636361692c8213d4c5b23d122a70fc872
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2025-02-03 17:44:46 -0300

    remove user-interaction

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -3,16 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './services/user.service';
 import { UserController } from './controllers/user.controller';
 import { User } from './entities/user.entity';
-import { UserInteraction } from './entities/user-interaction.entity';
-import { UserInteractionService } from './services/user-interaction.service';
-import { UserInteractionController } from './controllers/user-interaction.controller';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, UserInteraction]), // Registra a entidade User
+    TypeOrmModule.forFeature([User]), // Registra a entidade User
   ],
-  providers: [UserService, UserInteractionService],
-  controllers: [UserController, UserInteractionController],
+  providers: [UserService],
+  controllers: [UserController],
   exports: [UserService], // Exporte o serviço se outros módulos precisarem dele
 })
 export class UserModule {}
