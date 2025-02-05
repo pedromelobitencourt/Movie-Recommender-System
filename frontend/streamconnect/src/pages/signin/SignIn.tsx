956a639b5f6827b799002e0818bbea5f63a39024
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2025-02-05 18:18:58 -0300

    initial commit

--- frontend/streamconnect/src/pages/signin/SignIn.tsx
+++ frontend/streamconnect/src/pages/signin/SignIn.tsx
@@ -1,99 +1,95 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Alert, Spinner } from 'react-bootstrap';
 import { IonIcon } from '@ionic/react';
 import { mailOutline, lockClosedOutline } from 'ionicons/icons';
 import './SignIn.css';
 import { useAuth } from '../../Hooks/UseAuth';
 import { login } from '../../infra/usersDB';
 
-
 const SignIn: React.FC = () => {
   const { loginAuth } = useAuth();
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
-  const [userLogin, setUserLogin] = useState(false);
-  const [userLoginError, setUserLoginError] = useState(false);
-  const [customError, setCustomError] = useState("");
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, type, checked } = e.target;
-    setFormData({
-      ...formData,
-      [name]: type === 'checkbox' ? checked : value,
-    });
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('Form Data:', formData);
+    setIsLoading(true);
+    setErrorMessage(""); // Resetando erro ao enviar formulário
+
     try {
-    setUserLogin(true);
-    const token = await login(formData);
-    await loginAuth(token.access_token);
-    navigate('/catalog');
-    setUserLogin(false);
-    } catch (error: unknown) {
-      setCustomError((error as Error).message);
-      setUserLoginError(true);
+      const token = await login(formData);
+      await loginAuth(token.access_token);
+      navigate('/catalog');
+    } catch (error) {
+      setErrorMessage("E-mail ou senha incorretos. Tente novamente.");
     } finally {
-      setUserLogin(false);
+      setIsLoading(false);
     }
   };
 
   return (
-    <div>
-      <div className="wrapper">
-        <div className="form-box login">
-          <h2>Login</h2>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group className="input-box">
-              <label htmlFor="email">Email</label>
-              <div className="input-with-icon">
-                <IonIcon icon={mailOutline} className="input-icon" />
-                <Form.Control
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Digite seu email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-            </Form.Group>
+    <div className="wrapper">
+      <div className="form-box login">
+        <h2>Login</h2>
+        
+        {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
 
-            <Form.Group className="input-box">
-            <label htmlFor="password">Senha</label>
-              <div className="input-with-icon">
-                <IonIcon icon={lockClosedOutline} className="input-icon" />
-                <Form.Control
-                  type="password"
-                  id="password"
-                  name="password"
-                  placeholder="Digite sua senha"
-                  value={formData.password}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-            </Form.Group>
-
-            <Button type="submit" className="btn" disabled={userLogin}>
-              Login
-            </Button>
-
-            <div className="login-register">
-              <p>
-                Não tem uma conta? <a href="/signup" className="register-link">Registre-se</a>
-              </p>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="input-box">
+            <label htmlFor="email">E-mail</label>
+            <div className="input-with-icon">
+              <IonIcon icon={mailOutline} className="input-icon" />
+              <Form.Control
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Digite seu e-mail"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
             </div>
-          </Form>
-        </div>
+          </Form.Group>
+
+          <Form.Group className="input-box">
+            <label htmlFor="password">Senha</label>
+            <div className="input-with-icon">
+              <IonIcon icon={lockClosedOutline} className="input-icon" />
+              <Form.Control
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Digite sua senha"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </Form.Group>
+
+          <div className="remember-forgot">
+            <a href="/forgot-password">Esqueceu a senha?</a>
+          </div>
+
+          <Button type="submit" className="btn" disabled={isLoading}>
+            {isLoading ? <Spinner animation="border" size="sm" /> : "Entrar"}
+          </Button>
+
+          <div className="login-register">
+            <p>
+              Não tem uma conta? <a href="/signup" className="register-link">Cadastre-se</a>
+            </p>
+          </div>
+        </Form>
       </div>
     </div>
   );
